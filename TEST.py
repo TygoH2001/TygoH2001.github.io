3458bf8f7c5aa63d9db03f92480bee35586b33d5
TygoH2001 <144886823+TygoH2001@users.noreply.github.com>
2023-10-18 14:41:15 +0200

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,15 @@
+import pandas as pd
+#Change the path to the downloaded file
+xlsx = 'C:/Users/tygoh/Downloads/2020_jaarboek_941_4eccf4d8b6.xlsx'
+df = pd.read_excel(xlsx)
+
+#from txt to NaN, so it is all numeric
+df['totaal fosfor'] = pd.to_numeric(df['totaal fosfor'], errors='coerce')
+
+column1 = 'totaal fosfor'
+
+min_value = df[column1].min()
+max_value = df[column1].max()
+
+print(f"Minimum value in {column1} : {min_value}")
+print(f"Maximum value in {column1} : {max_value}")
